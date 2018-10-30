23a34e1e571c4a80562f15b14cbbbf53d9e37b57
Bersuch <bersuchdaniel@yahoo.de>
2018-10-30 13:55:46 +0100

    Add more Methods + Iterator

--- src/aufgabe1/SortedArrayDictionary.java
+++ src/aufgabe1/SortedArrayDictionary.java
@@ -1,158 +1,123 @@
 package Alda1;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class SortedArrayDictionary<K extends Comparable<? super K>, V> implements Dictionary<K, V> {
+public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
 
-	public SortedArrayDictionary(java.util.Comparator<? super K> cmp) {
-
-	}
-
-	private static class Entry<K,V> {
-		K key;
-		V value;
-		Entry<K,V>next;
-		Entry(K k, V v) {
-			key = k; value = v;
-		}
-	};
 	private static final int DEF_CAPACITY = 16;
 	private int size;
-	private Entry<K,V>[] data;
+	private Entry<K, V>[] data;
 
-
+	@SuppressWarnings("unchecked")
 	public SortedArrayDictionary() {
 		size = 0;
 		data = new Entry[DEF_CAPACITY];
 	}
 
+	@SuppressWarnings("unchecked")
 	private void ensureCapacity(int newCapacity) {
-		if (newCapacity < size) {
+
+		if (newCapacity < size)
 			return;
-		}
-		Entry<K, V>[] old = data;
+		Entry[] old = data;
 		data = new Entry[newCapacity];
 		System.arraycopy(old, 0, data, 0, size);
 	}
 
-	private int searchKey(K key) {
-		int li  = 0;
-		int re  = size -1;
-
-		while (re >= 1) {
-			int m =  (li + re) / 2;
-			if (key.compareTo(data[m].key) < 0)
-				re  = m - 1;
-			else if (key.compareTo(data[m].key) > 0 )
-				li  = m + 1;
-			else
-				return m; // key gefunden
-		}
-
-		return -1; // key nicht gefunden
-	}
-
-
-
 	@Override
 	public V insert(K key, V value) {
-
 		int i = searchKey(key);
-
-		// Vorhander Eintrag wird überschrieben
-
-		if (i != -1) {
-			V r = data[i].value;
-			data[i].value = value;
+		if (i != -1){
+			V r = data[i].getValue();
+			value = data[i].setValue(value);
 			return r;
 		}
-
-		// Neueintrag
 		if (data.length == size) {
-			ensureCapacity(2*size);
+			ensureCapacity(2 * size);
 		}
-
 		int j = size-1;
-		while (j >= 0 && key.compareTo(data[i].key) < 0) {
-			data[j + 1] = data[j];
+		Comparable<? super K> comparableKey = (Comparable<? super K>) key;
+		while (j >= 0 && comparableKey.compareTo(data[j].getKey()) < 0) {
+			data[j+1] = data[j];
 			j--;
 		}
-		data[j+1] = new Entry<K, V>(key, value);
+		data[j+1] = new Entry<K,V>(key,value);
 		size++;
 		return null;
 	}
 
-
-
+	private int searchKey(K key) {
+		int li = 0;
+		int re = this.size -1;
+		Comparable<? super K> comparableKey = (Comparable<? super K>) key;
+		
+		while (re >= li) {
+			int m = (li + re)/2;
+			if (comparableKey.compareTo(data[m].getKey())< 0)
+				re = m - 1;
+			else if (comparableKey.compareTo(data[m].getKey()) > 0)
+				li= m + 1;
+			else 
+				return m; // key gefunden
+			}
+		return -1; // key nicht gefunden
+	}
 
 	@Override
 	public V search(K key) {
-		int hash = getHashcode(key);
-		for (Entry<K, V> e = data[hash]; e != null; e = e.next) {
-			if (e.key.equals(key)) {
-				return e.value;
-			}
-		}
-		return null;
-	}
-
-	public int getHashcode(K key) {
-		int a = key.hashCode();
-		if(a<0)
-			a=-a;
-		a=a%data.length;
-		return a;
+		int i = searchKey(key);
+		if(i >= 0)
+			return data[i].getValue();
+		else
+			return null;
 	}
 
 	@Override
 	public V remove(K key) {
-		int hash = getHashcode(key);
-		V v = search(key);
-		if(v !=null)
-			for (Entry<K, V> e = data[hash]; e != null; e = e.next) {
-				if (e.key.equals(key)) {
-					data[hash] = e.next;
-					size--;
-					return e.value;
-
-				}
-
-			}
-		return null;
-
-
+		int i = searchKey(key);
+		if (i == -1)
+			return null;
+		// Datensatz loeschen und Lï¿½cke schlieï¿½en
+		V r = data[i].getValue();
+		for (int j = i; j < size-1; j++)
+			data[j] = data[j+1];
+		
+		data[--size] = null;
+		return r;
+		
 	}
 
 	@Override
 	public int size() {
+		// TODO Auto-generated method stub
 		return size;
 	}
 
 	@Override
-	// Don't forget to change your Package since we have different ones
-	public Iterator<Alda1.Dictionary.Entry<K, V>> iterator() {
+	public Iterator<Entry<K, V>> iterator() {
 		// TODO Auto-generated method stub
 		return new SortedArrayDictionaryIterator();
 	}
+	
+    private class SortedArrayDictionaryIterator implements Iterator<Entry<K,V>> {
+        private int current = 0;
 
-	// Here aswell or else it will give you an error
-	private class SortedArrayDictionaryIterator implements Iterator<Alda1.Dictionary.Entry<K,V>> {
-		private int current = 0;
+        public boolean hasNext(){
+        	
+            return current < size;
+        }
+        
+        public Entry<K,V> next() {   
+        	if (!(hasNext()))
+        		throw new NoSuchElementException();
+            return data[current++];
+        }
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 
-		public boolean hasNext() {
-			return current < size;
-		}
-
-		public Entry<K,V> next() {
-			if (!(hasNext()))
-				throw new NoSuchElementException();
-			return data[current++];
-		}
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
-	}
-
+	
 }
-
